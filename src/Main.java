27072e6a3b32bd7960bd5af03892f2441c2a1af0
Jasmijnvg <jasmijngenesen@gmail.com>
2024-05-29 14:59:20 +0200

    startup

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,39 @@
+    import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Hier mag je je code schrijven voor de hoofd-opdracht
+        Integer[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        String[] alphabetic = {"one", "two", "three", "four", "Five", "six", "seven", "eight", "nine", "zero"};
+
+        Translator translate = new Translator(alphabetic, numeric);
+
+        Boolean play = true;
+        String invalid = "invalid input";
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        while (play) {
+            System.out.println("Type 'x' te stop \nType 'v' to translate");
+            if (input.equals("x")) {
+                play = false;
+            } else if (input.equals("v")) {
+                System.out.println("Type a number between 0 to 9");
+            }
+
+            int number = scanner.nextInt();
+
+            if (number < 10) {
+                String result = translate.translate(number);
+                System.out.println("The translation of " + number + " is " + result);
+            } else {
+                System.out.println(invalid);
+            }
 
 
+        }
     }
 }
+
---
+++ src/Translator.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class Translator {
+    private HashMap<Integer, String> numericAlpha = new HashMap<>();
+
+    public Translator(String[] alphabetic, Integer[] numeric) {
+        for (int i = 0; i < alphabetic.length && i < numeric.length; i++){
+            numericAlpha.put(numeric[i], alphabetic[i]);
+        }
+    }
+
+    public HashMap<Integer, String> getNumericAlpha() {
+        return numericAlpha;
+    }
+
+    public void setNumericAlpha(HashMap<Integer, String> numericAlpha) {
+        this.numericAlpha = numericAlpha;
+    }
+
+    public String translate(Integer number) {
+        return numericAlpha.get(number);
+    }
+}
